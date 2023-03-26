7d8b13630cfa50f9ec8448884927e3a8231afe41
Pritish Mendhekar <39763520+PritishM1999@users.noreply.github.com>
2023-03-27 03:31:25 +0530

    backend changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_URL, {
+    const uri = process.env.DB_URL;
+    if (!uri) {
+      throw new Error('MongoDB URI is not defined');
+    }
+
+    const conn = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
     });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
@@ -16,6 +22,29 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.error(`Error: ${err.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 
 
 
